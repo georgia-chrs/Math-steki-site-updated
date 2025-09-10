c4288e5aaef92fb1c1e1a7126dbffc4f45f0de8d
georgia-chrs <georgia.chrysou.sec@gmail.com>
2025-09-11 01:55:19 +0300

    menuToggle

--- public/menu-drop.js
+++ public/menu-drop.js
@@ -16,6 +16,18 @@
       const nav = document.querySelector('nav');
       menuToggle.addEventListener('click', function() {
         nav.classList.toggle('open');
+         if (nav.classList.contains('open')) {
+          document.body.style.overflow = 'hidden';
+          document.documentElement.style.overflow = 'hidden'; // <-- πρόσθεσε αυτό
+          document.body.style.position = 'fixed'; // <-- και αυτό βοηθάει σε mobile
+          document.body.style.width = '100%';
+        } else {
+          document.body.style.overflow = '';
+          document.documentElement.style.overflow = '';
+          document.body.style.position = '';
+          document.body.style.width = '';
+        }
+
       });
 
 
